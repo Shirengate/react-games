fb8acc2b0445138db0192beeb33a959a307368e5
Shirengate <sweetgames551ggg@gmail.com>
2025-08-08 21:07:39 +0300

    configure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import instance from "./api/axios";
-
 export default function App() {
-  instance({
-    url: "/games",
-    method: "get",
-  }).then((e) => {
-    console.log(e);
-    return e;
-  });
-
-  return <div>App</div>;
+  return <>COLOR</>;
 }
